c99b2dd64704b35cc708e5a20f42fa00fd7ad9f4
Starry Skadi <starryskadi@gmail.com>
2020-08-26 20:55:18 +0630

    basic setup

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const index = props => {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
+
+index.propTypes = {
+
+}
+
+export default index
